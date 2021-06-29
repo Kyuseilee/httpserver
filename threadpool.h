58860b6eb188eacd48db5f59ab9c80efa2d0cef1
Kyuseilee <rosonlee98@gmail.com>
2021-06-29 19:53:48 +0800

    former modify

--- threadpool.h
+++ threadpool.h
@@ -4,3 +4,18 @@
  * @Last Modified by:   rosonlee 
  * @Last Modified time: 2021-06-17 21:47:39 
  */
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+template<class T>
+class threadpool{
+public:
+
+private:
+    int max_thread_number_;
+    std::queue<
+
+
+};
+
+#endif // THREADPOOL_H
